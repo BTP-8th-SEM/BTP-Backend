c1c1f4f56171d7f7b41e45c6babd6874f7ae6789
PrasadDalwee <prasaddalwee@gmail.com>
2023-04-24 21:31:17 +0530

    Changes for database

--- static/script.sql
+++ static/script.sql
@@ -1,33 +1,14 @@
-CREATE TABLE `mcq_options` (
+CREATE TABLE `users` (
+  `email` varchar(255) DEFAULT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `profilePicUrl` varchar(255) DEFAULT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
-  `option1` varchar(255) DEFAULT NULL,
-  `option2` varchar(255) DEFAULT NULL,
-  `option3` varchar(255) DEFAULT NULL,
-  `option4` varchar(255) DEFAULT NULL,
-  `correctOption` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `questions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `optionsId` int DEFAULT NULL,
-  `testId` int DEFAULT NULL,
-  `maxMarks` int DEFAULT NULL,
-  `body` varchar(255) DEFAULT NULL,
-  `topic` varchar(45) DEFAULT NULL,
-  `answerType` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `responses` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `questionId` int DEFAULT NULL,
-  `userId` int DEFAULT NULL,
-  `testId` int DEFAULT NULL,
-  `body` varchar(255) DEFAULT NULL,
-  `obtainedMarks` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE `test_db` (
@@ -44,14 +25,33 @@ CREATE TABLE `test_db` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE `users` (
-  `email` varchar(255) DEFAULT NULL,
-  `firstName` varchar(255) DEFAULT NULL,
-  `lastName` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `role` varchar(255) DEFAULT NULL,
-  `profilePicUrl` varchar(255) DEFAULT NULL,
+CREATE TABLE `questions` (
   `id` int NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `optionsId` int DEFAULT NULL,
+  `testId` int DEFAULT NULL,
+  `maxMarks` int DEFAULT NULL,
+  `body` varchar(255) DEFAULT NULL,
+  `topic` varchar(45) DEFAULT NULL,
+  `answerType` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `mcq_options` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `option1` varchar(255) DEFAULT NULL,
+  `option2` varchar(255) DEFAULT NULL,
+  `option3` varchar(255) DEFAULT NULL,
+  `option4` varchar(255) DEFAULT NULL,
+  `correctOption` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `responses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `questionId` int DEFAULT NULL,
+  `userId` int DEFAULT NULL,
+  `testId` int DEFAULT NULL,
+  `body` varchar(255) DEFAULT NULL,
+  `obtainedMarks` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
