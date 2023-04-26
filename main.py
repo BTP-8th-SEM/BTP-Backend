35cf83cd9670122c3fc03e3fd8bac23400b4ed1c
shreyas-085 <shreyask085@gmail.com>
2023-04-26 13:17:07 +0530

    tags 2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
-    "http://localhost",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
