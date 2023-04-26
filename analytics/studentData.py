6eb722677e0a6513cdbc037fb3ba753f21012851
PrasadDalwee <prasaddalwee@gmail.com>
2023-04-26 20:39:21 +0530

    student data analysis implemented

---
+++ analytics/studentData.py
@@ -0,0 +1,26 @@
+from analytics.connection import dataframes
+from analytics.questionwise_OLAP import OLAP_questions
+from analytics.testwise_OLAP import OLAP_Test
+
+
+#analyzes test scores wrt topics
+class studentAnalysis:
+    testId:int
+    df:dataframes
+    def __init__(self,df:dataframes) -> None:
+        self.df=df
+        
+    def getTestData(self, testId:int, studentId: int):
+        olapTest=OLAP_Test(df=self.df,testId=testId)
+        results=olapTest.calculate()
+        responses=self.df.responses.query('testId==@testId & userId==@studentId')
+        results['studentId']=studentId
+        results['totalMarksObtained']=responses['obtainedMarks'].sum()
+        return results
+    
+    def getQuestionData(self, testId:int, studentId: int, questionId: int):
+        olapq=OLAP_questions(df=self.df,testId=testId)
+        result=olapq.calculate(questionId=questionId)
+        response=self.df.responses.query('testId==@testId & userId==@studentId & questionId==@questionId')
+        result['obtainedMarks']=response['obtainedMarks'].iloc[0]
+        return result
--- static/print_tables.sql
+++ static/print_tables.sql
@@ -3,6 +3,5 @@ select * from test_db;
 select * from student_test_map;
 select * from questions;
 select * from mcq_options;
-select * from responses;
 select * from olap_test;
 select * from olap_question;
