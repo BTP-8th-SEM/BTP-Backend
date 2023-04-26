30f642de75139d6b332bca17eccf83a3e8d0f6f3
shreyas-085 <shreyask085@gmail.com>
2023-04-26 10:59:42 +0530

    olap

---
+++ routes/OLAPApis.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from analytics.OLAP import run_OLAP_Test
+
+router = APIRouter()
+        
+@router.get("/getAnalysisTestById/{id}")
+def get_user_by_email(id: int):
+   return run_OLAP_Test(id)
