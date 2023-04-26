25a35db47a7fb0f503a8e9236de59654559efede
PrasadDalwee <prasaddalwee@gmail.com>
2023-04-27 02:37:21 +0530

    Topic wise analysis done

--- analytics/studentData.py
+++ analytics/studentData.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from analytics.connection import dataframes
 from analytics.questionwise_OLAP import OLAP_questions
 from analytics.testwise_OLAP import OLAP_Test
@@ -9,7 +10,8 @@ class studentAnalysis:
     df:dataframes
     def __init__(self,df:dataframes) -> None:
         self.df=df
-        
+    
+    #provides overall analysis of test wrt student's total score    
     def getTestData(self, testId:int, studentId: int):
         olapTest=OLAP_Test(df=self.df,testId=testId)
         results=olapTest.calculate()
@@ -18,6 +20,7 @@ class studentAnalysis:
         results['totalMarksObtained']=responses['obtainedMarks'].sum()
         return results
     
+    #provides analysis of particular question wrt student's response to that question
     def getQuestionData(self, testId:int, studentId: int, questionId: int):
         olapq=OLAP_questions(df=self.df,testId=testId)
         result=olapq.calculate(questionId=questionId)
@@ -25,7 +28,8 @@ class studentAnalysis:
         result['obtainedMarks']=response['obtainedMarks'].iloc[0]
         return result
     
-    def getStudentsList(self, testId:int):
+    #provides list of students that appeared in given Test
+    def getStudents(self,testId:int):
         responses=self.df.responses.query('testId==@testId')
         userIdList=responses.userId.unique()
         results=[]
@@ -37,4 +41,37 @@ class studentAnalysis:
             data['lastName']=user['lastName']
             data['obtainedMarks']=self.getTestData(testId=testId,studentId=id)['totalMarksObtained']
             results.append(data)
+        return results
+    
+    #provides topic-wise analysis of the Test wrt student's responses
+    def getTopicAnalysis(self, testId:int, userId:int):
+        questions = self.df.questions.query('testId==@testId')
+        responses = self.df.responses.query('testId==@testId')
+        topics=questions['topic'].unique()
+        
+        def data(topic:str,averageMarks:float,yourMarks:int,highestMarks:int,lowestMarks:int):
+            body={}
+            body['topic']=topic
+            body['averageMarks']=averageMarks
+            body['yourMarks']=yourMarks
+            body['highestMarks']=highestMarks
+            body['lowestMarks']=lowestMarks
+            return body
+
+        merged_df = questions.merge(responses, left_on='id', right_on='questionId')
+        results=[]
+        
+        merged_df = pd.merge(questions, responses, left_on='id', right_on='questionId')
+
+        user_marks_sum = merged_df.groupby(['topic', 'userId'])['obtainedMarks'].agg('sum').reset_index()
+        most_marks_by_user = user_marks_sum.groupby('topic').agg('max').reset_index()
+        avg_marks_by_topic = merged_df.groupby('topic')['obtainedMarks'].agg('mean').reset_index()
+        least_marks_by_user = user_marks_sum.groupby('topic').agg('min').reset_index()
+        
+        for topic in topics:
+            yourMarks=user_marks_sum.query('topic==@topic & userId==@userId')['obtainedMarks'].iloc[0]
+            highestMarks=most_marks_by_user.query('topic==@topic')['obtainedMarks'].iloc[0]
+            averageMarks=avg_marks_by_topic.query('topic==@topic')['obtainedMarks'].iloc[0]
+            lowestMarks=least_marks_by_user.query('topic==@topic')['obtainedMarks'].iloc[0]
+            results.append(data(topic=topic,averageMarks=averageMarks,yourMarks=yourMarks,highestMarks=highestMarks,lowestMarks=lowestMarks))
         return results
